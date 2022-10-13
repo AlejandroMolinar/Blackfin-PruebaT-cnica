9dbdce0a15222bda1225fa378ed2fc1306b38a0b
AlejandroMolinar <alejandro.molinar17@gmail.com>
2022-10-13 09:59:40 -0400

    Init

---
+++ src/Ejercicio1.java
@@ -0,0 +1,10 @@
+/***********************************************************************************
+ * Escribe, en el lenguaje de programación que desees, un programa que muestre     *
+ * en pantalla los números del 1 al 100, sustituyendo los múltiplos de 3 por el    *
+ * palabro “VIN” y, a su vez, los múltiplos de 5 por “CLE”. Para los casos que, al *
+ * tiempo, son múltiplos de 3 y 5, utiliza el combinado “VINCLE”.                  *
+ ***********************************************************************************/
+
+git remote add origin https://github.com/AlejandroMolinar/Blackfin-PruebaTecnica.git
+git branch -M main
+git push -u origin main
